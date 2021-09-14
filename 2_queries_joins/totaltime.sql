a476d869b38540cb520baac48869f8dfe06a4417
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-09-13 23:56:03 -0700

    BootcampX Queries 3 - Group By

--- 2_queries_joins/totaltime.sql
+++ 2_queries_joins/totaltime.sql
@@ -4,3 +4,13 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
+
+-- SELECT count(assignment_submissions) as total_submissions
+-- FROM assignment_submissions;
+
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL 
+-- GROUP BY students.name
+-- HAVING count(assignment_submissions.*) < 100;
